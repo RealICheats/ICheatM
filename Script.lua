d691179a7ba47c170bf546a8d9a4c61cb8fc133c
RealICheats <isaacglitch2011@gmail.com>
2026-03-07 16:51:51 -0600

    Update Rayfield source URL in Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "ICheat",
